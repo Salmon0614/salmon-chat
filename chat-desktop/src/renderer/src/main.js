16d072b793c5d3d52c3505510541a384b4a8be87
Salmon <1179732961@qq.com>
2024-06-03 14:25:15 +0800

    引入iconfont

--- chat-desktop/src/renderer/src/main.js
+++ chat-desktop/src/renderer/src/main.js
@@ -1,7 +1,12 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+
 import 'element-plus/dist/index.css'
+import '@/assets/cust-elementplus.scss'
+import '@/assets/base.scss'
+import '@/assets/icon/iconfont.css'
+
 import router from '@/router'
 
 const app = createApp(App)
