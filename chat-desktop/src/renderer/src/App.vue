e03a1d9b17a1d69dd0f611c8ff13d1a03985a9f1
Salmon <1179732961@qq.com>
2024-06-03 12:13:56 +0800

    初始化项目（引入 element、router、国际化）

--- chat-desktop/src/renderer/src/App.vue
+++ chat-desktop/src/renderer/src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
-import Versions from './components/Versions.vue'
+import {ref, reactive, getCurrentInstance, nextTick} from 'vue'
+
+const {proxy} = getCurrentInstance()
+import {ElConfigProvider} from 'element-plus'
+
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+const locale = zhCn;
+
+const config = reactive({
+  max: 1
+})
 </script>
 
 <template>
-  <div>我的第一个桌面应用</div>
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+
+  </el-config-provider>
 </template>
 
 <style lang="less">
---
+++ chat-desktop/src/renderer/src/views/Login.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref, reactive, getCurrentInstance, nextTick} from 'vue'
+
+const {proxy} = getCurrentInstance()
+
+const formData = ref({});
+const formDataRef = ref();
+const rules = {
+  title: [{required: true, message: "请输入内容"}],
+}
+</script>
+
+<template>
+  <div>
+    <el-form :model="formData" :rules="rules" ref="formDataRef"
+             label-width="80px" @submit.prevent>
+      <el-form-item label="邮箱" prop="email">
+        <el-input clearable placeholder="提示信息" v-model.trim="formData.email">
+
+        </el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<style scoped lang="less">
+
+</style>
