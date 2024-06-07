b7a2bc4c0d42aca2654cd245cc4327280c9e2f9a
Salmon <1179732961@qq.com>
2024-06-07 20:18:09 +0800

    返回是否是管理员

--- chat-service/src/main/java/com/salmon/chatService/model/dto/account/EmailRegisterRequest.java
+++ chat-service/src/main/java/com/salmon/chatService/model/dto/account/EmailRegisterRequest.java
@@ -6,8 +6,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 /**
  * 注册（邮箱方式）
@@ -22,15 +21,16 @@ public class EmailRegisterRequest extends BaseCheckCode {
 
     @Schema(description = "邮箱")
     @NotBlank(message = "请输入邮箱")
-    @Pattern(regexp = "^([A-Za-z0-9_\\-\\.])+\\@([A-Za-z0-9_\\-\\.])+\\.([A-Za-z]{2,4})$",message = "请输入正确的邮箱")
+    @Email(message = "请输入正确的邮箱")
     private String email;
 
     @Schema(description = "密码")
     @NotBlank(message = "请输入密码")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "密码至少8个字符，包含大小写字母和数字")
     private String password;
 
     @Schema(description = "昵称")
     @NotBlank(message = "请填写昵称")
-    @Length(max = 10, message = "昵称长度不能超过10位")
+    @Length(min = 2, max = 10, message = "昵称长度在2~10之间")
     private String nickname;
 }
