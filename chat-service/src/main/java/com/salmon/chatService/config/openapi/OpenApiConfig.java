da68a9b7fdbb14388c415bb8b12e5b20492503f3
Salmon <1179732961@qq.com>
2024-06-08 23:51:22 +0800

    openapi增加携带请求头token

--- chat-service/src/main/java/com/salmon/chatService/config/openapi/OpenApiConfig.java
+++ chat-service/src/main/java/com/salmon/chatService/config/openapi/OpenApiConfig.java
@@ -3,6 +3,9 @@ package com.salmon.chatService.config.openapi;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.HeaderParameter;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,5 +23,25 @@ public class OpenApiConfig {
                 .contact(new Contact().name("Salmon").url("https://chat.salmonedu.ltd").email("1179732961@qq.com"))
                 .description("Salmon Chat（类似微信、QQ的聊天服务）")
                 .version("v1.0.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi tokenApi(OperationCustomizer operationCustomizer) {
+        return GroupedOpenApi.builder()
+                .group("鉴权")
+                .packagesToScan("com.salmon.chatService.controller")
+                .pathsToMatch("/**")
+                .addOperationCustomizer(operationCustomizer)
+                .build();
+    }
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        return (operation, handlerMethod) -> operation.addParametersItem(
+                new HeaderParameter()
+                        .name("token")
+                        .required(false)
+                        .description("token 验证")
+        );
     }
 }
