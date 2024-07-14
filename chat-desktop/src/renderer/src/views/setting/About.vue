3c3082ca11942d458caefdffd6b5aa59d322f9b5
Salmon <1179732961@qq.com>
2024-07-15 03:14:53 +0800

    关于（待完善）

--- chat-desktop/src/renderer/src/views/setting/About.vue
+++ chat-desktop/src/renderer/src/views/setting/About.vue
@@ -1,7 +1,35 @@
-<script setup></script>
+<script setup>
+import { ref, reactive, getCurrentInstance, nextTick, onMounted } from 'vue'
+import config from '../../../../../package.json'
+const { proxy } = getCurrentInstance()
+const formData = ref({})
+const formDataRef = ref()
+
+const rules = {
+  password: [{ required: true, message: '请输入新密码' }]
+}
+
+// console.log(config.version)
+
+/**
+ * todo 检查更新
+ */
+const checkUpdate = () => {}
+</script>
 
 <template>
-  <div>关于</div>
+  <ContentPanel>
+    <el-form ref="formDataRef" :model="formData" :rules="rules" @submit.prevent>
+      <el-form-item label="版本信息">
+        <div class="version-info">
+          <div>SalmonChat {{ config.version }}</div>
+          <div>
+            <el-button type="primary" @click="checkUpdate">检查更新</el-button>
+          </div>
+        </div>
+      </el-form-item>
+    </el-form>
+  </ContentPanel>
 </template>
 
 <style scoped lang="scss"></style>
